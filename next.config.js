8e2c65f2a96e7927326ee704ac832d7589965a45
kavinmmcloudshiftsolutions <kavin@cloudshiftsolutions.in>
2026-08-25 19:58:08 +0530

    chore: add mongoose and mongodb to serverExternalPackages in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  serverExternalPackages: ['mongoose', 'mongodb'],
   images: {
     remotePatterns: [
       {
